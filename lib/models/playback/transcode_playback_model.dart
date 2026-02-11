b530d9cd690ed1347678eed6c619691737264bfc
Joseph Gershgorin <jgershgorin@biocentury.com>
2026-02-11 09:46:39 -0800

    Added native Windows ARM64 support - Fix formatting/linting issues

--- lib/models/playback/transcode_playback_model.dart
+++ lib/models/playback/transcode_playback_model.dart
@@ -31,28 +31,40 @@ class TranscodePlaybackModel extends PlaybackModel {
   });
 
   @override
-  List<SubStreamModel> get subStreams => [SubStreamModel.no(), ...mediaStreams?.subStreams ?? []];
+  List<SubStreamModel> get subStreams =>
+      [SubStreamModel.no(), ...mediaStreams?.subStreams ?? []];
 
-  List<jellyfin.QueueItem> get itemsInQueue =>
-      queue.mapIndexed((index, element) => jellyfin.QueueItem(id: element.id, playlistItemId: "playlistItem$index")).toList();
+  List<jellyfin.QueueItem> get itemsInQueue => queue
+      .mapIndexed((index, element) => jellyfin.QueueItem(
+          id: element.id, playlistItemId: "playlistItem$index"))
+      .toList();
 
   @override
-  Future<TranscodePlaybackModel> setSubtitle(SubStreamModel? model, MediaControlsWrapper player) async {
+  Future<TranscodePlaybackModel> setSubtitle(
+      SubStreamModel? model, MediaControlsWrapper player) async {
     final newIndex = await player.setSubtitleTrack(model, this);
-    return copyWith(mediaStreams: () => mediaStreams?.copyWith(defaultSubStreamIndex: newIndex));
+    return copyWith(
+        mediaStreams: () =>
+            mediaStreams?.copyWith(defaultSubStreamIndex: newIndex));
   }
 
   @override
-  List<AudioStreamModel> get audioStreams => [AudioStreamModel.no(), ...mediaStreams?.audioStreams ?? []];
+  List<AudioStreamModel> get audioStreams =>
+      [AudioStreamModel.no(), ...mediaStreams?.audioStreams ?? []];
 
   @override
-  Future<TranscodePlaybackModel>? setAudio(AudioStreamModel? model, MediaControlsWrapper player) async {
+  Future<TranscodePlaybackModel>? setAudio(
+      AudioStreamModel? model, MediaControlsWrapper player) async {
     final newIndex = await player.setAudioTrack(model, this);
-    return copyWith(mediaStreams: () => mediaStreams?.copyWith(defaultAudioStreamIndex: newIndex));
+    return copyWith(
+        mediaStreams: () =>
+            mediaStreams?.copyWith(defaultAudioStreamIndex: newIndex));
   }
 
   @override
-  Future<TranscodePlaybackModel>? setQualityOption(Map<Bitrate, bool> map) async => copyWith(bitRateOptions: map);
+  Future<TranscodePlaybackModel>? setQualityOption(
+          Map<Bitrate, bool> map) async =>
+      copyWith(bitRateOptions: map);
 
   @override
   Future<PlaybackModel?> playbackStarted(Duration position, Ref ref) async {
@@ -77,7 +89,8 @@ class TranscodePlaybackModel extends PlaybackModel {
   }
 
   @override
-  Future<PlaybackModel?> playbackStopped(Duration position, Duration? totalDuration, Ref ref) async {
+  Future<PlaybackModel?> playbackStopped(
+      Duration position, Duration? totalDuration, Ref ref) async {
     ref.read(playBackModel.notifier).update((state) => null);
 
     await ref.read(jellyApiProvider).sessionsPlayingStoppedPost(
@@ -93,7 +106,8 @@ class TranscodePlaybackModel extends PlaybackModel {
   }
 
   @override
-  Future<PlaybackModel?> updatePlaybackPosition(Duration position, bool isPlaying, Ref ref) async {
+  Future<PlaybackModel?> updatePlaybackPosition(
+      Duration position, bool isPlaying, Ref ref) async {
     final api = ref.read(jellyApiProvider);
     await api.sessionsPlayingProgressPost(
       body: jellyfin.PlaybackProgressInfo(
@@ -125,7 +139,8 @@ class TranscodePlaybackModel extends PlaybackModel {
   }
 
   @override
-  String toString() => 'TranscodePlaybackModel(item: $item, playbackInfo: $playbackInfo)';
+  String toString() =>
+      'TranscodePlaybackModel(item: $item, playbackInfo: $playbackInfo)';
 
   @override
   TranscodePlaybackModel copyWith({
@@ -145,7 +160,8 @@ class TranscodePlaybackModel extends PlaybackModel {
       media: media != null ? media() : this.media,
       playbackInfo: playbackInfo ?? this.playbackInfo,
       mediaStreams: mediaStreams != null ? mediaStreams() : this.mediaStreams,
-      mediaSegments: mediaSegments != null ? mediaSegments() : this.mediaSegments,
+      mediaSegments:
+          mediaSegments != null ? mediaSegments() : this.mediaSegments,
       chapters: chapters != null ? chapters() : this.chapters,
       trickPlay: trickPlay != null ? trickPlay() : this.trickPlay,
       queue: queue ?? this.queue,
